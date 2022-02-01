78f45c0bc29a4657f482e43eba8f7257d1e3189c
Maxim Kozhevnikov <kozhevnikovmn@gmail.com>
2022-01-31 23:18:19 -0500

    createProduct

--- test/marketplace.test.js
+++ test/marketplace.test.js
@@ -1,8 +1,11 @@
 const { assert } = require("chai");
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
 
 const Marketplace = artifacts.require("./Marketplace.sol");
 
-contract("Marketplace", (accounts) => {
+contract("Marketplace", ([deployer, seller, buyer]) => {
   let marketplace;
 
   before(async () => {
@@ -22,6 +25,52 @@ contract("Marketplace", (accounts) => {
     it("has a name", async () => {
       const name = await marketplace.name();
       assert.equal(name, "Marketplace Name");
+    });
+  });
+
+  describe("products", async () => {
+    let result, productCount;
+    before(async () => {
+      result = await marketplace.createProduct(
+        "iPhone 13",
+        web3.utils.toWei("1", "Ether"),
+        // metadata
+        { from: seller }
+      );
+      productCount = await marketplace.productCount();
+    });
+
+    it("creates products", async () => {
+      assert.equal(productCount, 1);
+
+      // console.log(result.logs);
+
+      const event = result.logs[0].args;
+
+      assert.equal(
+        event.id.toNumber(),
+        productCount.toNumber(),
+        "id is correct"
+      );
+      assert.equal(event.name, "iPhone 13", "name is correct");
+      assert.equal(event.price, "1000000000000000000", "price is correct");
+      assert.equal(event.owner, seller, "owner is correct");
+      assert.equal(event.purchased, false, "purchased is correct");
+
+      // FAILURE
+      await marketplace.createProduct(
+        "",
+        web3.utils.toWei("1", "Ether"),
+        // metadata
+        { from: seller }
+      ).should.be.rejected;
+
+      await marketplace.createProduct(
+        "iPhone 13",
+        0,
+        // metadata
+        { from: seller }
+      ).should.be.rejected;
     });
   });
 });
